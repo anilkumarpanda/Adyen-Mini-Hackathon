0c1c39019befd9d8d96f84a231d45017de28c113
anilkumarpanda <anilkumarpanda@users.noreply.github.com>
2017-11-01 01:26:50 +0100

    Added comments

--- adyen_fraud.py
+++ adyen_fraud.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
 
 # NOTE: Make sure that the class is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,8 +11,11 @@ features = tpot_data.drop('target', axis=1).values
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'].values, random_state=42)
 
-# Score on the training set was:0.85
-exported_pipeline = RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.7000000000000001, min_samples_leaf=2, min_samples_split=8, n_estimators=100)
+# Score on the training set was:0.875
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=ExtraTreesClassifier(bootstrap=False, criterion="entropy", max_features=0.9000000000000001, min_samples_leaf=20, min_samples_split=12, n_estimators=100)),
+    RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.25, min_samples_leaf=7, min_samples_split=16, n_estimators=100)
+)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
